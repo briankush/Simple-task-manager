87d400ba482b60c540aefdf1b5b08bdb5015b447
briankush <briankush05@gmail.com>
2025-06-19 09:48:36 +0300

    Task page almost complete

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import{ useState } from 'react';
 import Task from './components/Task';
 
 export default function App(){
-  const[tasks, setTasks] = usestate([
+  const[tasks, setTasks] = useState([
     {id: 1, title:"Write Lesson Plan", completed: false},
-    {id: 2, title:"Review MongoDb Quiz", complete: true}
+    {id: 2, title:"Review MongoDb Quiz", completed: true}
   ]);
   const toggleTask = id => {
     setTasks(curr => curr.map (t => t.id === id ? {...t, completed: !t.completed } : t));
@@ -13,8 +13,8 @@ export default function App(){
   return(
     <div className="min-h-screen bg-gray-100 p-6">
       <h1 className="text-3xl font-bold mb-4">Task Dashboard</h1>
-      {tasks.map(tasks =>(
-        <Task key= {tasks.id} {...task} onToggle={toggleTask}/>
+      {tasks.map(task => (
+        <Task key={task.id} {...task} onToggle={toggleTask} />
       ))}
     </div>
   )
